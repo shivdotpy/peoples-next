4e618cccf1b3a26705be02d75e77b0129d71e501
SHIV SHANKAR <shivshankar@SHIVs-MacBook-Pro.local>
2023-06-06 13:14:13 +0530

    initial landing page

--- app/page.test.tsx
+++ app/page.test.tsx
@@ -3,7 +3,32 @@ import Home from './page';
 import RootLayout from './layout';
 
 describe('Home Page', () => {
-  it('Renders home page and show text Peoples', () => {
+  test('renders the heading', () => {
     render(<Home />);
+    const heading = screen.getByText('Welcome to peoples page');
+    expect(heading).toBeInTheDocument();
+  });
+
+  test('renders the link with correct text content', () => {
+    render(<Home />);
+    const link = screen.getByText('Explore Peoples');
+    expect(link).toBeInTheDocument();
+    expect(link.getAttribute('href')).toBe('/peoples');
+  });
+
+  test('renders the image with correct attributes', () => {
+    render(<Home />);
+    const image = screen.getByAltText('peoples');
+    expect(image).toBeInTheDocument();
+    expect(image).toHaveAttribute('height', '500');
+    expect(image).toHaveAttribute('width', '500');
+  });
+
+  test('renders the footer text', () => {
+    render(<Home />);
+    const footer = screen.getByText(
+      '© 2023 Peoples App. No rights reserved. 😂'
+    );
+    expect(footer).toBeInTheDocument();
   });
 });
